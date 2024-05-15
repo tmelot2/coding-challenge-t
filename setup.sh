feeed4adeb7be7ca2857a512c929a05abb748504
Ted Melot <tmelot@gmail.com>
2024-05-15 10:14:33 -0400

    Move app config to .env file, use it with setup container

--- setup.sh
+++ setup.sh
@@ -8,4 +8,4 @@ set -euo pipefail
 setupName="query_tool_db_setup"
 
 docker build -f ./Dockerfile-setup -t $setupName .
-docker run -it --rm --name $setupName $setupName
+docker run -it --rm --env-file .env --name $setupName $setupName
